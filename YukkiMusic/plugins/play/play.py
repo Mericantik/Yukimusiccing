609d3d268102d15ea71b050c61f3272e8e4e6cb4
Inopem Adika <49522880+npdkdev@users.noreply.github.com>
2023-10-28 18:15:04 +0700

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -336,7 +336,15 @@ async def play_commnd(
             return await mystic.delete()
         else:
             try:
+                await app.send_message(
+                    config.LOG_GROUP_ID,
+                    "Mencoba untuk memutar url yang tidak dikenal...",
+                )
                 await Yukki.stream_call(url)
+                await app.send_message(
+                    config.LOG_GROUP_ID,
+                    "Berhasil memutar url yang tidak dikenal...",
+                )
             except NoActiveGroupCall:
                 await mystic.edit_text(
                     "There's an issue with the bot. Please report it to my owner and ask them to check logger group."
@@ -346,10 +354,18 @@ async def play_commnd(
                     "Please turn on Voice Chat.. Bot is not able to stream urls..",
                 )
             except Exception as e:
-                print(traceback.format_exc())
+                print(traceback.format_exc(), e)
+                await app.send_message(
+                    config.LOG_GROUP_ID,
+                    f"Mencoba untuk memutar url yang tidak dikenal...{traceback.format_exc()}",
+                )
                 return await mystic.edit_text(
                     _["general_3"].format(type(e).__name__)
                 )
+            await app.send_message(
+                config.LOG_GROUP_ID,
+                f"Finish untuk memutar url yang tidak dikenal...}",
+            )
             await mystic.edit_text(_["str_2"])
             try:
                 await stream(
